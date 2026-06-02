8be893c2166e13be49b8a77a9ee40587e558bd7e
DmitriyKrasnoshekov <dimitriy216@gmail.com>
2026-06-02 21:22:47 +0000

    покрыли тестами всю карточку, а код комментами

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 from success_class.success import success
 from typing import List
-print("Zero second version!")
+#print("Zero second version!")
 
 
 class Card:
@@ -48,7 +48,7 @@ class Card:
 					else:
 						contains_value = True
 	
-		# Если имя не строка, имя - пустая строка или нет ни одного корректного, то карточка недействительна
+		# Если имя - не строка, имя - пустая строка или нет ни одного корректного значения, то карточка недействительна
 		if type(name) != str or name == '' or not contains_value:
 			self.name = None
 			self.usual_vals = []
@@ -59,9 +59,12 @@ class Card:
 			self.success.set_unsuccessful()
 			return
 		
+		# Если имя - непустая строка
 		if type(name) == str and name != '':
+			# Если было хотя бы одно неправильное значение
 			if exists_uncorrect_value:
 				self.success.set_half_successful()
+			# Если все были правильные
 			else:
 				self.success.set_successful()
 				return
@@ -105,7 +108,7 @@ card1 = add_card("<number>",
 				[],
                 [],
 				[])
-print(type(card1),'\n')
+#print(type(card1),'\n')
 card2 = Card("<number>",
 				["1", "2"],
 				[						
@@ -116,9 +119,19 @@ card2 = Card("<number>",
 				[],
                 [],
 				[])
-print(card2.get_card())
+#print(card2.get_card())
 
 def imitate(correct_name: bool, correct, incorrect, few_vals: int, all_vals: int, few_types: int):
+	"""
+	correct_name : bool - является ли имя корректным
+	correct - что вставлять в качестве корректного значения или имена
+	incorrect - что вставлять в качестве НЕкорректного значения или имена
+	few_vals - количество корректный значений в типе
+	all_vals - общее количество значений в типе
+	few_types - количество корректных типов
+	"""
+
+	# устанавливаем корректное или некорректное имя
 	if correct_name:
 		name = correct
 	else:
@@ -134,6 +147,7 @@ def imitate(correct_name: bool, correct, incorrect, few_vals: int, all_vals: int
             selflink_vals, templ_vals, selflink_templ_vals,
             id_vals]
 	
+	# Формируем значения для ввода в Card
 	for i, one_type in enumerate(vals_types):
 		if i < few_types:
 			for j in range(all_vals):
@@ -143,14 +157,19 @@ def imitate(correct_name: bool, correct, incorrect, few_vals: int, all_vals: int
 					one_type += [incorrect]
 		else:
 			vals_types[i] = incorrect
-		
+
+	# выводим список типов значений	
 	#print('val_types = ', vals_types)
+
+	# формируем карточку
 	new_card = Card(name, usual_vals,
             selflink_vals, templ_vals, selflink_templ_vals,
             id_vals)
 
+	# печатаем карточку
 	print(new_card.get_card())
 
+	# если имя карточки некорректно, зануляем все значения
 	if not correct_name:
 		name = None
 		usual_vals = []
@@ -159,6 +178,7 @@ def imitate(correct_name: bool, correct, incorrect, few_vals: int, all_vals: int
 		selflink_templ_vals = []
 		id_vals = []
 	
+	# Формируем ожидаемый вывод теста
 	expected = f'status=unsuccessful\n'
 
 	expected += f'name={name}\n'
@@ -167,7 +187,27 @@ def imitate(correct_name: bool, correct, incorrect, few_vals: int, all_vals: int
 	expected += f'templ_vals={templ_vals}\n'
 	expected += f'selflink_templ_vals={selflink_templ_vals}\n'
 	expected += f'id_vals={id_vals}\n'
-	print(expected)
-	print(expected == new_card.get_card())
 
-imitate(False, None, 'c++', 0, -1, 0)
+	# выводим ожидаемый вывод теста 
+	print(expected)
+
+	# сравниваем ожидаемый вывод теста и реальный вывод
+	print(expected == new_card.get_card())
+	
+	return expected == new_card.get_card()
+
+
+# Если имя - не строка, имя - пустая строка или нет ни одного корректного значения, то карточка недействительна
+#imitate(False, None, 'c++', 0, -1, 0)
+#imitate(False, '', 'c++', 0, -1, 0)
+imitate(True, 'good', None, 0, 0, 2)
+
+# Если имя - непустая строка И
+# Если было хотя бы одно неправильное значение
+#imitate(True, 'good', None, 1, 4, 1)
+#imitate(True, 'good', None, 2, 4, 2)
+#imitate(True, 'good', None, 4, 5, 2)
+
+# Если имя - непустая строка И
+# Если все были правильные
+#imitate(True, 'good', None, 4, 4, 5)
