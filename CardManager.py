7cac75d141cf8657b677947a7d136e5a17a4e4b1
DmitriyKrasnoshekov <dimitriy216@gmail.com>
2026-07-28 19:55:06 +0000

    плохая сохраненка validate_value_type

--- CardManager.py
+++ CardManager.py
@@ -1,9 +1,11 @@
 import json
+from collections.abc import Callable
 from notification_classes import common_notification
 from Card import Card
 
 class CardManager():
 	def __init__(self):
+		self.name = None
 		self.libruary = [] # хранит объекты класс Card
 		self.notifications = common_notification.common_notification() # хранит ошибки, которые произошли при создании карточек
 	
@@ -19,14 +21,20 @@ class CardManager():
 		'''
 		return {"val": value_str}
 
-	def validate_value_type(self, value_type, value_str: str, func, values_list, inner): # какого типа func, inner?
+	def validate_value_type(self, depth: int, user_value_type: str, real_value_type_str: str,\
+							check_value_type_func: Callable[[str,str],bool],\
+							value:str,\
+							one_type_values_list: list, inner_func: Callable):
 		# Сделать замыканием?
-		if value_type == value_str and func(<параметры>):
-			informated_val_value = self.gen_value_links_info(value_str)
-			values_list.append(informated_val_value)
+		if user_value_type == real_value_type_str and check_value_type_func(self.name, value):
+			informated_val_value = self.gen_value_links_info(real_value_type_str)
+			one_type_values_list.append(informated_val_value)
+			return True
 		else:
-			value_type = ''
-			inner(<value_type?>) # рекурсивно вызываем замыкание, чтобы проверить другие типы значений
+			# value_type == '' когда юзер ошибся
+			user_value_type = ''
+			inner_func(depth + 1) # рекурсивно вызываем замыкание, чтобы проверить другие типы значений
+			return False
 			  
 	def values_validate(self, values: list):
 		# инициализируем списки для каждого типа значений
@@ -39,55 +47,60 @@ class CardManager():
 		id_templ_vals = []
 		id_selflink_templ_vals = []
 
-		# проходим по каждому значению и определяем его тип
-		for i, one_value in enumerate(values):
-			# проверяем, что значение имеет правильный формат 
-			# (должно быть словарем с ключами "type" и "value")
-			try:
-				value_type = one_value.get('type')
-				value_str = one_value.get('value')
-			except AttributeError:
-				self.notifications.add_error_with_stack_nodes(f"Invalid format of value #{i+1}:\n{json.dumps(one_value, indent=4)}")
-				continue
+		
 
-			# Внутренняя функция (замыкание). 
-			# Она имеет доступ ко всем переменным, объявленным выше, и в цикле ниже.
-			def __inner():
-				if value_type is None:
-						self.notifications.add_error_with_stack_nodes(f"Unknown type of value #{i+1}:\n{json.dumps(one_value, indent=4)}")
-				
-				elif value_type == '' or value_type == 'selflink':
-					self.validate_value_type(value_type, 'selflink', self.is_selflink_value, selflink_vals, __inner)
-			  
-			  	elif value_type == '' or value_type == 'usual':
-					self.validate_value_type(value_type, 'usual', self.is_usual_value, usual_vals, __inner)
-
-				elif value_type == '' or value_type == 'template':
-					self.validate_value_type(value_type, 'template', self.is_template_value, template_vals, __inner)
-
-				elif value_type == '' or value_type == 'selflink-template':
-					self.validate_value_type(value_type, 'selflink-template', self.is_selflink-template_value, selflink-template_vals, __inner)
-				
-				elif value_type == '' or value_type == 'id':
-					self.validate_value_type(value_type, 'id', self.is_id_value, id_vals, __inner)
-				
-				elif value_type == '' or value_type == 'id-selflink':
-					self.validate_value_type(value_type, 'id-selflink', self.is_id_selflink_value, id_selflink_vals, __inner)
-				
-				elif value_type == '' or value_type == 'id-template':
-					self.validate_value_type(value_type, 'id-template', self.is_id_template_value, id_template_vals, __inner)
-				
-				elif value_type == '' or value_type == 'id-selflink-template':
-					self.validate_value_type(value_type, 'id-selflink-template', self.is_id_selflink_template_value, id_selflink_template_vals, __inner)
-				
-				# elif value_type == '':
-				# 	return
-				# возможно выход из рекурсии происходит автоматом после последнего действия в рабочей ветке
-
-				else:
+		# Внутренняя функция (замыкание). 
+		# Она имеет доступ ко всем переменным, объявленным выше, и в цикле ниже.
+		# depth начинается с нуля. В первом вызове depth=0
+		# value_type == '' когда юзер ошибся
+		def __inner(depth: int):
+			if depth>1:
+				return
+			
+			elif type(value_type) != str:
 					self.notifications.add_error_with_stack_nodes(f"Unknown type of value #{i+1}:\n{json.dumps(one_value, indent=4)}")
+					return
+			
+			self.validate_value_type(depth, value_type, 'selflink', self.is_selflink_value, value_str, selflink_vals, __inner)
+			self.validate_value_type(depth, value_type, 'usual', self.is_usual_value, value_str, usual_vals, __inner)
+			self.validate_value_type(depth, value_type, 'template', self.is_template_value, value_str, templ_vals, __inner)
+			self.validate_value_type(depth, value_type, 'selflink-template', self.is_selflink_templ_value, value_str, selflink_templ_vals, __inner)
+			self.validate_value_type(depth, value_type, 'id', self.is_id_value, value_str, id_vals, __inner)
+			self.validate_value_type(depth, value_type, 'id-selflink', self.is_id_selflink_value, value_str, id_selflink_vals, __inner)
+			self.validate_value_type(depth, value_type, 'id-template', self.is_id_template_value, value_str, id_templ_vals, __inner)
+			self.validate_value_type(depth, value_type, 'id-selflink-template', self.is_id_selflink_template_value, value_str, id_selflink_templ_vals, __inner)
 
-'''
+			# elif value_type == '' or value_type == 'selflink':
+			# 	self.validate_value_type(depth, value_type, 'selflink', self.is_selflink_value, value_str, selflink_vals, __inner)
+			
+			# elif value_type == '' or value_type == 'usual':
+			# 	self.validate_value_type(depth, value_type, 'usual', self.is_usual_value, value_str, usual_vals, __inner)
+
+			# elif value_type == '' or value_type == 'template':
+			# 	self.validate_value_type(depth, value_type, 'template', self.is_template_value, value_str, template_vals, __inner)
+
+			# elif value_type == '' or value_type == 'selflink-template':
+			# 	self.validate_value_type(depth, value_type, 'selflink-template', self.is_selflink-template_value, value_str, selflink-template_vals, __inner)
+			
+			# elif value_type == '' or value_type == 'id':
+			# 	self.validate_value_type(depth, value_type, 'id', self.is_id_value, value_str, id_vals, __inner)
+			
+			# elif value_type == '' or value_type == 'id-selflink':
+			# 	self.validate_value_type(depth, value_type, 'id-selflink', self.is_id_selflink_value, value_str, id_selflink_vals, __inner)
+			
+			# elif value_type == '' or value_type == 'id-template':
+			# 	self.validate_value_type(depth, value_type, 'id-template', self.is_id_template_value, value_str, id_template_vals, __inner)
+			
+			# elif value_type == '' or value_type == 'id-selflink-template':
+			# 	self.validate_value_type(depth, value_type, 'id-selflink-template', self.is_id_selflink_template_value, value_str, id_selflink_template_vals, __inner)
+			
+			# elif value_type == '':
+			# 	return
+			# возможно выход из рекурсии происходит автоматом после последнего действия в рабочей ветке
+
+			else:
+				self.notifications.add_error_with_stack_nodes(f"Unknown type of value #{i+1}:\n{json.dumps(one_value, indent=4)}")
+		'''
 			def __inner():
 				if value_type is None:
 						self.notifications.add_error_with_stack_nodes(f"Unknown type of value #{i+1}:\n{json.dumps(one_value, indent=4)}")
@@ -151,9 +164,20 @@ class CardManager():
 					#	__inner(<параметр глубины>) # рекурсивно вызываем замыкание, чтобы проверить другие типы значений
 				else:
 					self.notifications.add_error_with_stack_nodes(f"Unknown type of value #{i+1}:\n{json.dumps(one_value, indent=4)}")
-'''
+		'''
+
+		# проходим по каждому значению и определяем его тип
+		for i, one_value in enumerate(values):
+			# проверяем, что значение имеет правильный формат 
+			# (должно быть словарем с ключами "type" и "value")
+			try:
+				value_type = one_value.get('type')
+				value_str = one_value.get('value')
+			except AttributeError:
+				self.notifications.add_error_with_stack_nodes(f"Invalid format of value #{i+1}:\n{json.dumps(one_value, indent=4)}")
+				continue
 			# вызываем замыкание для добавления значения в нужный список
-			__inner()
+			__inner(0)
 		
 		# формируем и возвращаем валидированные значения
 		# (Важно: возврат должен быть здесь, после завершения цикла)
@@ -172,11 +196,13 @@ class CardManager():
 
 	def create_card(self, name: str, values: list):
 		# ловим ошибку типа параметра name и values
+		
 		got_error = False
 		if not isinstance(name, str):
 			self.notifications.add_error_with_stack_nodes(f"Invalid format of card name:\n{name}")
 			got_error = True
-	
+		self.name = name
+
 		if not isinstance(values, list):
 			self.notifications.add_error_with_stack_nodes(f"Invalid format of card values:\n{json.dumps(values, indent=4)}")
 			got_error = True
@@ -209,29 +235,31 @@ class CardManager():
 		# и класть карточку в библиотеку
 		pass
 
+	# Сигнатура is_функций одинаковая, даже если не все аргументы нужны,
+	# потому что мы передаем is_функцию в value_validate_type как аргумент
 	def is_selflink_value(self, name: str, value: str):
 		return name in value
 
-	def is_template_value(self):
+	def is_template_value(self, name: str, value: str):
 		pass
 
-	def is_selflink_template_value(self):
-		return self.is_selflink_value() and self.is_template_value()
+	def is_selflink_template_value(self, name: str, value: str):
+		return self.is_selflink_value(name, value) and self.is_template_value(name, value)
 
-	def is_id_value(self):
+	def is_id_value(self, name: str, value: str):
 		# Uses Parser to determine if the value is an ID
 		pass
 
-	def is_id_template_value(self):
-		return self.is_id_value() and self.is_template_value()
+	def is_id_template_value(self, name: str, value: str):
+		return self.is_id_value(name, value) and self.is_template_value(name, value)
 
-	def is_id_selflink_value(self):
-		return self.is_id_value() and self.is_selflink_value()
+	def is_id_selflink_value(self, name: str, value: str):
+		return self.is_id_value(name, value) and self.is_selflink_value(name, value)
 
-	def is_id_selflink_template_value(self):
-		return self.is_id_value() and self.is_selflink_value() and self.is_template_value()
+	def is_id_selflink_template_value(self, name: str, value: str):
+		return self.is_id_value(name, value) and self.is_selflink_value(name, value) and self.is_template_value(name, value)
 
-	def is_usual_value(self):
+	def is_usual_value(self, name: str, value: str):
 		# использует 
 		# is_self_link_value
 		# is_template_value
